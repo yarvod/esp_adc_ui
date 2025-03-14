ba89f550febdbd770165dc5ff0465f62203e9cd2
Прохорова Юлия <y.prokhorova@glowbyteconsulting.com>
2025-03-14 14:12:20 +0300

    jj

--- build-win.bat
+++ build-win.bat
@@ -1,4 +1,4 @@
 pyinstaller main.py --clean -n EspAdc --onedir --icon="./assets/volt64.png" --noconsole -y --add-data="assets:assets"
-copy .\settings.ini .\dist\EspAdc\settings.ini
+@REM copy .\settings.ini .\dist\EspAdc\settings.ini
 mkdir .\dist\EspAdc\assets
 copy .\assets\* .\dist\EspAdc\assets
