8336af099e727417646a4345bf6b75de76a2c366
Прохорова Юлия <y.prokhorova@glowbyteconsulting.com>
2025-03-14 14:08:12 +0300

    fix

--- api/esp_adc.py
+++ api/esp_adc.py
@@ -62,7 +62,7 @@ class EspAdc(BaseInstrument):
         self.write(f"hostFile=/{file}")
         with open(file, "wb") as file:
             while True:
-                data = self.adapter.socket.recv(32768)
+                data = self.adapter.socket.recv(4096)
                 if not data:
                     return f"File {file} downloaded"
                 file.write(data)
